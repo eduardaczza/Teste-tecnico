52cbdb0f7b273f8fe17a25f90e32176e0792699e
eduardaczza <165608058+eduardaczza@users.noreply.github.com>
2024-09-01 18:38:07 -0300

    teste

---
+++ Teste/ex1.java
@@ -0,0 +1,18 @@
+package Teste;
+
+public class ex1 {
+    public static void main(String[] args) {
+
+        int indice = 13;
+
+        int soma = 0;
+        
+        int k = 0;
+
+        while (k < indice) {
+            k = k + 1;
+            soma = soma + k;
+            }
+        System.out.println("O valor da soma é igual a: " + soma);
+    }
+}
